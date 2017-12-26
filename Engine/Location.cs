7f0e1ec347ea6cf967618d93d01d3637fbe5c9c0
Franck-PC\Franck <franck.martin3d@gmail.com>
2017-12-26 15:56:24 +1100

    9.1 working

--- Engine/Location.cs
+++ Engine/Location.cs
@@ -11,12 +11,22 @@ namespace Engine
         public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public Item ItemRequiredToEnter { get; set; }
+        public Quest QuestAvailbaleHere { get; set; }
+        public Monster MonsterLivingHere { get; set; }
+        public Location LocationToNorth { get; set; }
+        public Location LocationToEast { get; set; }
+        public Location LocationToSouth { get; set; }
+        public Location LcoationToWest { get; set; }
 
-        public Location(int id, string name, string description)
+        public Location(int id, string name, string description, Item itemRequiredtoEnter = null, Quest questAvailableHere = null, Monster monsterLivingHere = null )
         {
             ID = id;
             Name = name;
             Description = description;
+            ItemRequiredToEnter = itemRequiredtoEnter;
+            QuestAvailbaleHere = questAvailableHere;
+            MonsterLivingHere = monsterLivingHere;
         }
 
 
