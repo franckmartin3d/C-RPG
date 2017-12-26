bf17f3ca52cc6168f0051bf2cfd2ccbdccbdb61c
Franck-PC\Franck <franck.martin3d@gmail.com>
2017-12-26 16:30:42 +1100

    10.1 finish

--- Engine/Quest.cs
+++ Engine/Quest.cs
@@ -15,6 +15,8 @@ namespace Engine
         public int RewardGold { get; set; }
         public Item RewardItem { get; set; }
 
+        public List<QuestCompletionItem>QuestCompletionItems { get; set; }
+
         public Quest(int id, string name, string description, int rewardExperiencePoints, int rewardGold)
         {
             ID = id;
@@ -22,6 +24,8 @@ namespace Engine
             Description = description;
             RewardExperiencePoints = rewardExperiencePoints;
             RewardGold = rewardGold;
+
+            QuestCompletionItems = new List<QuestCompletionItem>();
         }
 
 }
